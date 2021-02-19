789015450238a35340fb886070e0939732d43325
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-02-19 11:17:35 -0500

    commit tree acdaa3fbad39f87b23e9de38012aaadb7ab97a70

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+attr_reader :roster
+
+
+  def initialize(name)
+   @name = name
+   @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+       @roster[grade] << student
+    else
+       @roster[grade] = []
+       @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+   @roster[grade]
+  end
+
+  def sort
+   @roster.transform_values {|values| values.sort}
+  end
+
+end
